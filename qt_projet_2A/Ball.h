1b6ca9bc52904a6b1420a7dce20f2511edc24b12
kevin mainguene <mainguene.kevin@gmail.com>
2016-05-27 12:27:33 +0200

    Ajout de la class Ball

--- qt_projet_2A/Ball.h
+++ qt_projet_2A/Ball.h
@@ -1,22 +1,69 @@
-#pragma once
-#include <string>
-using namespace std;
+#include "Ball.h"
 
-class Ball
+
+Ball::Ball()
 {
-public:
-	Ball();
-	~Ball();
+}
 
-	int getXPos();
-	void setXPos(int x);
+Ball::Ball(String name){
+	
+	setType(name);
+	if (name == "vert"){
+		setHSVmin(Scalar(70, 24, 121));
+		setHSVmax(Scalar(100, 126, 255));
 
-	int getYPos();
-	void setYPos(int y);
+		setColour(Scalar(0, 255, 0));
+	}
+	if (name == "rouge"){
+		setHSVmin(Scalar(0, 0, 0));
+		setHSVmax(Scalar(5, 5, 255));
 
-private:
-	int xPos, yPos;
-	string type;
+		setColour(Scalar(0, 0, 255));
+	}
+	if (name == "blanc"){
+		setHSVmin(Scalar(0, 30, 225));
+		setHSVmax(Scalar(17, 80, 255));
 
-};
+		setColour(Scalar(255, 255, 255));
+	}
+}
 
+Ball::~Ball()
+{
+}
+
+int Ball::getXPos(){
+
+	return Ball::xPos;
+}
+
+void Ball::setXPos(int x){
+
+	Ball::xPos = x;
+}
+int Ball::getYPos(){
+
+	return Ball::yPos;
+}
+
+void Ball::setYPos(int y){
+
+	Ball::yPos = y;
+}
+
+Scalar Ball::getHSVmin(){
+
+	return Ball::HSVmin;
+}
+Scalar Ball::getHSVmax(){
+
+	return Ball::HSVmax;
+}
+void Ball::setHSVmin(Scalar min){
+
+	Ball::HSVmin = min;
+}
+void Ball::setHSVmax(Scalar max){
+
+	Ball::HSVmax = max;
+}
